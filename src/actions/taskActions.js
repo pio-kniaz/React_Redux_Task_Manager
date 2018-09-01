5352d68cda181bf0d986830631df30d20f4a0577
pio-kniaz <pio.kniaz@gmail.com>
2018-09-01 23:40:12 +0200

    app connected with firebase GetTask and AddTask

--- src/actions/taskActions.js
+++ src/actions/taskActions.js
@@ -1,22 +1,36 @@
-import { GET_TASKS,DELETE_TASK,ADD_TASK,DONE_TASK } from "./types";
-export const getTasks=()=>{
-	return{
-		type:GET_TASKS
-	}
-}
-export const deleteTask=(id)=>{
-	return{
-		type:DELETE_TASK,payload:id
-	}
-}
-export const addTask=(newTask)=>{
-	return{
-		type:ADD_TASK,payload:newTask
-	}
-}
+import { GET_TASKS, DELETE_TASK, ADD_TASK, DONE_TASK } from "./types";
+import axios from "axios";
+export const getTasks = () => async dispatch => {
+  const res = await axios.get(
+    "https://contact-702be.firebaseio.com/Tasks.json"
+  );
+  let tasksFromFireBase = [];
+  for (var variable in res.data) {
+    tasksFromFireBase.push(res.data[variable]);
+  }
+  dispatch({
+    type: GET_TASKS,
+    payload: tasksFromFireBase
+  });
+};
+export const deleteTask = id => {
+  return {
+    type: DELETE_TASK,
+    payload: id
+  };
+};
 
-export const doneTask=(taskId)=>{
-	return{
-		type:DONE_TASK,payload:taskId
-	}
-}
+export const addTask = newTask => async dispatch=>{
+	const post = await axios.post('https://contact-702be.firebaseio.com/Tasks.json',newTask)
+  dispatch ({
+    type: ADD_TASK,
+    payload: newTask
+  });
+};
+
+export const doneTask = taskId => {
+  return {
+    type: DONE_TASK,
+    payload: taskId
+  };
+};
--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,27 +1,27 @@
 import { GET_TASKS,DELETE_TASK,ADD_TASK,DONE_TASK } from "../actions/types";
 const initialState = {
   tasks: [
-    {
-      category: "REDUX" + Math.floor(Math.random() * 51),
-      id: 0,
-      done: true,
-      date: new Date(),
-      description: "LoremLorem0"
-    },
-    {
-      category: "Test REdux" + Math.floor(Math.random() * 51),
-      id: 1,
-      done: false,
-      date: new Date(),
-      description: "LoremLorem1"
-    },
-    {
-      category: "TEst Redux" + Math.floor(Math.random() * 51),
-      id: 2,
-      done: false,
-      date: new Date(),
-      description: "LoremLorem2"
-    }
+    // {
+    //   category: "REDUX" + Math.floor(Math.random() * 51),
+    //   id: 0,
+    //   done: true,
+    //   date: new Date(),
+    //   description: "LoremLorem0"
+    // },
+    // {
+    //   category: "Test REdux" + Math.floor(Math.random() * 51),
+    //   id: 1,
+    //   done: false,
+    //   date: new Date(),
+    //   description: "LoremLorem1"
+    // },
+    // {
+    //   category: "TEst Redux" + Math.floor(Math.random() * 51),
+    //   id: 2,
+    //   done: false,
+    //   date: new Date(),
+    //   description: "LoremLorem2"
+    // }
   ]
 };
 
@@ -29,7 +29,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case GET_TASKS:
       return {
-        ...state
+        ...state,
+				tasks:action.payload
       };
       break;
 		case DELETE_TASK:
